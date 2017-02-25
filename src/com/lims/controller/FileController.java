e7f4a2728edfd02e641ddde9dc3ed9dd683082ad
chenyycwh <caiwenhong@promote.cache-dns.local>
2017-02-25 21:58:49 +0800

    监测频次列表，环境要素

---
+++ src/com/lims/controller/FileController.java
@@ -0,0 +1,30 @@
+package com.lims.controller;
+
+import com.jfinal.core.Controller;
+import com.jfinal.kit.FileKit;
+import com.jfinal.upload.UploadFile;
+import com.lims.utils.RenderUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+/**
+ * Created by caiwenhong on 2017/2/25.
+ */
+public class FileController extends Controller {
+    public void upload() {
+        try {
+            UploadFile uploadFile = getFile();
+            Map result = RenderUtils.codeFactory(200);
+            String path = " /upload\\" + uploadFile.getFileName();
+            System.out.println(path);
+            result.put("path", path);
+            renderJson(result);
+        } catch (Exception e) {
+            renderError(500);
+        }
+    }
+}
