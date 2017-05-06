9b43329a930ef4185a2d5abff62df7d54bcece70
chenyycwh <782307419@qq.com>
2017-05-06 08:39:24 +0800

    新增项目
    新增项目

--- src/com/lims/controller/CompanyController.java
+++ src/com/lims/controller/CompanyController.java
@@ -1,5 +1,6 @@
 package com.lims.controller;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.jfinal.core.Controller;
 import com.jfinal.json.Jackson;
 import com.jfinal.plugin.activerecord.Db;
@@ -167,11 +168,8 @@ public class CompanyController extends Controller {
             ExcelRead read = new ExcelRead();
             String[] titles = {"id", "company", "element", "pointList", "projectList", "frequency"};
             List<Map> reading = read.readExcel(path, titles);
-            List returnBack = new ArrayList();
-            Map<String, List> obj = new HashMap<>();
             Boolean result = true;
             for (Map temp : reading) {
-
                 String companyStr = temp.get("company").toString();
                 String elementStr = temp.get("element").toString();
                 String frequencyStr = temp.get("frequency").toString();
@@ -182,41 +180,96 @@ public class CompanyController extends Controller {
                 Company company = new Company();
                 if (getParaToInt("flag") == 1) {
                     List<Company> companyList = Company.companydao.find("select * from `db_company` where company = " + companyStr + "And task_id =" + getPara("task_id"));
+                    Contractitem contractitem = new Contractitem();
                     if (companyList != null) {
+                        company.set("process", 0);
+                        result = result && company.update();
+                        contractitem.set("company_id", company.getInt("id")).set("point", pointList.length).set("other", companyStr).set("element", element).set("frequency", frequency.toJsonSingle());
+                        result = result && contractitem.save();
+                        for (String projectName : projectList) {
+                            MonitorProject project = MonitorProject.monitorProjectdao.findFirst("SELECT * FROM `db_monitor_project` WHERE name='" + projectName + "'");
+                            if (project != null) {
+                                Integer[] project1 = getParaValuesToInt("project[]");
+                                for (Integer pro : project1) {
+                                    ItemProject itemProject = new ItemProject();
+                                    result = result && itemProject.set("project_id", pro).set("item_id", contractitem.get("id")).set("isPackage", itemProject.get("isPackage")).save();
+                                }
+                            }
+                        }
 
-                        company.set("company", companyStr).set("task_id", getPara("task_id")).set("flag", 1)
-                                .set("creater", ParaUtils.getCurrentUser(getRequest()).getInt("id"))
-                                .set("create_time", ParaUtils.sdf2.format(new Date())).set("process", 0)
-                                .save();
-                        result = result && company.save();
                     } else {
                         company.set("company", companyStr).set("task_id", getPara("task_id")).set("flag", 1)
                                 .set("creater", ParaUtils.getCurrentUser(getRequest()).getInt("id"))
-                                .set("create_time", ParaUtils.sdf2.format(new Date())).set("process", 0)
-                                .save();
+                                .set("create_time", ParaUtils.sdf2.format(new Date())).set("process", 0);
                         result = result && company.save();
+                        contractitem.set("company_id", company.getInt("id")).set("point", pointList.length).set("other", companyStr).set("element", element).set("frequency", frequency.toJsonSingle());
+                        result = result && contractitem.save();
+                        for (String projectName : projectList) {
+                            MonitorProject project = MonitorProject.monitorProjectdao.findFirst("SELECT * FROM `db_monitor_project` WHERE name='" + projectName + "'");
+                            if (project != null) {
+
+                                ItemProject itemProject = new ItemProject();
+                                result = result && itemProject.set("project_id", project.getInt("id")).set("item_id", contractitem.get("id")).set("isPackage", itemProject.get("isPackage")).save();
+
+                            }
+                        }
                     }
 
                 } else if (getParaToInt("flag") == 0) {
                     List<Company> companyList = Company.companydao.find("select * from `db_company` where company = " + companyStr + "And contract_id =" + getPara("contract_id"));
+                    Contractitem contractitem = new Contractitem();
                     if (companyList != null) {
-                        company.set("company", companyStr).set("contract_id", getPara("contract_id")).set("flag", 0)
-                                .set("creater", ParaUtils.getCurrentUser(getRequest()).getInt("id"))
-                                .set("create_time", ParaUtils.sdf2.format(new Date())).set("process", 0)
-                                .save();
-                        result = result && company.save();
+                        company.set("process", 0);
+                        result = result && company.update();
+                        for (String projectName : projectList) {
+                            MonitorProject project = MonitorProject.monitorProjectdao.findFirst("SELECT * FROM `db_monitor_project` WHERE name='" + projectName + "'");
+                            if (project != null) {
+                                ItemProject itemProject = new ItemProject();
+                                result = result && itemProject.set("project_id", project.getInt("id")).set("item_id", contractitem.get("id")).set("isPackage", itemProject.get("isPackage")).save();
+
+                            }
+                        }
                     } else {
                         company.set("company", companyStr).set("contract_id", getPara("contract_id")).set("flag", 0)
                                 .set("creater", ParaUtils.getCurrentUser(getRequest()).getInt("id"))
-                                .set("create_time", ParaUtils.sdf2.format(new Date())).set("process", 0)
-                                .save();
+                                .set("create_time", ParaUtils.sdf2.format(new Date())).set("process", 0);
+                        result = result && company.save();
+                        for (String projectName : projectList) {
+                            MonitorProject project = MonitorProject.monitorProjectdao.findFirst("SELECT * FROM `db_monitor_project` WHERE name='" + projectName + "'");
+                            if (project != null) {
+                                ItemProject itemProject = new ItemProject();
+                                result = result && itemProject.set("project_id", project.getInt("id")).set("item_id", contractitem.get("id")).set("isPackage", itemProject.get("isPackage")).save();
+                            }
+                        }
                     }
 
                 }
             }
+            renderJson(result ? RenderUtils.CODE_SUCCESS : RenderUtils.CODE_ERROR);
         } catch (Exception e) {
             renderError(500);
         }
+    }
 
+    /***
+     * 修改监测项目
+     * **/
+
+    public void changeItem() {
+        try {
+            int item_id = getParaToInt("id");
+            Contractitem contractitem = Contractitem.contractitemdao.findById(item_id);
+            Integer[] project = getParaValuesToInt("project[]");
+            boolean result = true;
+            if (contractitem != null) {
+                List<ItemProject> itemProjectList = ItemProject.itemprojectDao.find("SELECT p.* FROM `db_item` i,`db_item_project`p WHERE id =" + item_id + "And p.item_id=i.id");
+                Integer[] list =(Integer[])itemProjectList.toArray();
+
+            }
+
+
+        } catch (Exception e) {
+            renderError(500);
+        }
     }
 }
