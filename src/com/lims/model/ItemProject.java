40806c96d29d3ef17b5111405c6ca3b2c867376e
chenyycwh <782307419@qq.com>
2017-05-27 16:21:21 +0800

    实验室分析

--- src/com/lims/model/ItemProject.java
+++ src/com/lims/model/ItemProject.java
@@ -20,9 +20,12 @@ public class ItemProject extends Model<ItemProject> {
             }
             temp.put(name, monitorProject.get(name));
         }
+        temp.put("project_id",this.get("project_id"));
+        temp.put("id",this.get("id"));
         temp.put("isPackage", this.get("isPackage"));
         temp.put("item_project_id", this.get("id"));
         temp.put("process", this.get("process"));
+        temp.put("flag",this.get("flag"));
         Inspect inspect = Inspect.inspectDao.findFirst("SELECT * FROM `db_inspect` WHERE item_project_id=" + this.get("id"));
         temp.put("inspect", inspect);
 //        return monitorProject.toJsonSingle();
