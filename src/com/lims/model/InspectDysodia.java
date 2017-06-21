17e5467856f539380a380aec665481db82eb7aa3
chenyycwh <782307419@qq.com>
2017-06-21 19:10:25 +0800

    办公室统计功能
    办公室统计功能

--- src/com/lims/model/InspectDysodia.java
+++ src/com/lims/model/InspectDysodia.java
@@ -19,6 +19,9 @@ public class InspectDysodia extends Model<InspectDysodia> {
         temp.put("inspect_id", this.get("inspect_id"));
         temp.put("process", this.get("process"));
         temp.put("type", "dysodia");
+        temp.put("flag", this.get("flag") == null ? null : this.get("flag"));
+        temp.put("flag2", this.get("flag2") == null ? null : this.get("flag2"));
+        temp.put("flag3", this.get("flag3") == null ? null : this.get("flag3"));
         return temp;
     }
 }
